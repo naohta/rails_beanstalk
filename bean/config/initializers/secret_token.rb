b321ba57ec1c41027e5f295f4ec7f7ed628503fe
naohta <naohta@gmail.com>
2013-04-11 10:31:39 +0900

    1st

---
+++ bean/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bean::Application.config.secret_token = 'c873fe8a077ab2ade0aba960e09484f7a4784b41f6f34cae2e9a08e0db2da99e4e6713fa06d14bea55662313b837253fbaae637cb8c9e398f284070f37098df2'
